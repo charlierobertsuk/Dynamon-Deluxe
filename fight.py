85e7d3800bd265765dd1be21eb2474ea3585c63c
Charlie Roberts <charlie@robertsclan.co.uk>
2024-01-15 14:36:10 +0000

    cr-create-game-window

--- fight.py
+++ fight.py
@@ -3,3 +3,8 @@ import random # this imports random - randomness is key in a game like this
 import time # this imports time - for time between attacks and stuff
 import math # this imports math
 
+pygame.init()
+
+# the game window
+screen = pygame.display.set_mode((1280,720))
+pygame.display.set_caption("FIGHT!")
