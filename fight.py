0d624513a8b65f92498d8930c0bbe9c9211a029b
Charlie Roberts <charlie@robertsclan.co.uk>
2024-01-15 18:28:23 +0000

    cr-dynamon-class

--- fight.py
+++ fight.py
@@ -26,3 +26,36 @@ class Move():
         self.name = name # name of the move
         self.power = power # power of move -- the amount of damage dealt
         self.type = type # type as in like water, fire and grass
+
+class Dynamon(pygame.sprite.Sprite):
+
+    def __init__(self, name, level, x, y):
+        
+        pygame.sprite.Sprite.__init__(self)
+
+        # set name and level of Dynamon
+        self.name = name
+        self.level = level
+
+        # sprite position
+        self.x = x
+        self.y = y
+
+        # start number of healing spray
+        self.num_healspray = 3
+
+        # set predefined stats of the dynamon
+        self.current_hp = 100 + self.level
+        self.max_hp = 100 + self.level
+        self.attack = 50
+        self.defence = 30
+        self.speed = 40
+
+        # set dynamon types
+        self.types = ["normal"]
+
+        # set sprite width
+        self.size = 150
+
+        # set sprite to thefront facing sprite
+        self.set_sprite("front_default")
